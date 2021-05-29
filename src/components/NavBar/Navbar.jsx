e362fb9bd4769121fc7d6a104353bad5cdf9075c
Edgar <cardenasedgar123@gmail.com>
2021-05-29 16:36:45 -0400

    Created Navbar component

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -8,13 +8,36 @@ import {
   Menu,
   Typography,
 } from "@material-ui/core";
-import { ShoppingCar } from "@material-ui/icons";
+import { ShoppingCart } from "@material-ui/icons";
+import classes from "*.module.css";
+
+import logo from "../../assets/store.png";
 
 const Navbar = () => {
   return (
-      <>
-        
-      </>
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.appBar} color="inherit">
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Stuff 'n Stuff
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
